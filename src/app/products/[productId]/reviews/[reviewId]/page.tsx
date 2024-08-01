f7d0b39f2857f0276011a4d0f3c01bc93e305dd5
amontalvof <amontalvof92@gmail.com>
2024-07-31 22:42:10 -0400

    feat: Not Found Page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation';
+
 const Review = ({
     params,
 }: {
     params: { productId: string; reviewId: string };
 }) => {
+    if (parseInt(params.reviewId) > 1000) {
+        return notFound();
+    }
     return (
         <div>
             Review {params.productId} product {params.reviewId}
